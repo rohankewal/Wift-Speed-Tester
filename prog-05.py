454e8542008c98d660d4dd0c3800a303dc7d8020
Rohan Kewalramani <45906368+rohankewal@users.noreply.github.com>
2019-12-24 11:37:13 -0500

    Initial Commit to Master Branch

---
+++ prog-05.py
@@ -0,0 +1,35 @@
+import speedtest
+
+def test():
+    s = speedtest.Speedtest()
+    s.get_servers()
+    s.get_best_server()
+    s.download()
+    s.upload()
+    res = s.results.dict()
+    return res["download"], res["upload"], res["ping"]
+
+def main():
+    with open('test.csv', 'w') as f:
+        f.write('download,upload,ping\n')
+        for i in range(3):
+            print('Making test #{}'.format(i + 1))
+            d, u, p = test()
+            f.write('{},{},{}\n'.format(d, u, p))
+    with open('test.txt', 'w') as f:
+        for i in range(3):
+            print('Making test #{}'.format(i+1))
+            d, u, p = test()
+            f.write('Test #{}\n'.format(i+1))
+            f.write('Download: {:.2f} Kb/s\n'.format(d / 1024))
+            f.write('Upload: {:.2f} Kb/s\n'.format(u / 1024))
+            f.write('Ping: {}\n'.format(p))
+    for i in range(3):
+        d, u, p = test()
+        print('Test #{}\n'.format(i+1))
+        print('Download: {:.2f} Kb/s\n'.format(d / 1024))
+        print('Upload: {:.2f} Kb/s\n'.format(u / 1024))
+        print('Ping: {}\n'.format(p))
+
+if __name__ == '__main__':
+    main()
